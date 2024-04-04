5c6c6470262615d9e4e18b2b4202b9db960db0fe
Gourav Kumar <gourav.kumar@thoughtworks.com>
2024-04-04 17:00:03 +0530

    add: middleware for validating errors

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,7 +1,6 @@
 import express, { Request, Response } from "express";
-import { body, validationResult } from "express-validator";
-import { RequestValidationError } from "../errors/request-validation-error";
-import { DatabaseConnectionError } from "../errors/database-connection-error";
+import { body } from "express-validator";
+import { validateRequest } from "../middleware/validate-request";
 
 const router = express.Router();
 
@@ -11,18 +10,11 @@ router.post(
     body("email").isEmail().withMessage("Email must be valid"),
     body("password")
       .trim()
-      .isLength({ min: 4, max: 20 })
-      .withMessage("Password must be between 4 and 20"),
+      .notEmpty()
+      .withMessage("You must enter a password!"),
   ],
+  validateRequest,
   (req: Request, res: Response) => {
-    const errors = validationResult(req);
-
-    if (!errors.isEmpty()) {
-      throw new RequestValidationError(errors.array());
-    }
-
-    throw new DatabaseConnectionError();
-
     const { email, password } = req.body;
     res.send({
       email,
